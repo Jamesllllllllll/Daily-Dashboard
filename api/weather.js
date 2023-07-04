cd0e44eaa92a31282013df42583e4e35857b3d25
James <jkeezer@pm.me>
2023-07-04 15:57:35 -0400

    Switch Serverless to Fetch (not Axios)

--- api/weather.js
+++ api/weather.js
@@ -1,3 +1,26 @@
+/************ Fetch version  ************/
+
+export default async function handler(request, response) {
+
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+  const { city } = request.query; // or .queryStringParameters ?
+  const params = "&q=" + city + "&days=1&aqi=no&alerts=no";
+
+  try {
+    const res = await fetch(`${url}${params}`);
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+      return response.send(JSON.stringify(data));
+    }
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+/************ Axios version ************
+ 
 const axios = require("axios");
 
 export default async function handler(request, response) {
@@ -18,3 +41,5 @@ export default async function handler(request, response) {
     }
   }
 };
+
+ ***************************************/
