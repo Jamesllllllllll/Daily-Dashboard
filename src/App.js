732ffce7e3bffcf4246a3bc8671707d1c780b548
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-04 21:52:42 +1000

    Add Notes component to app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Weather from './features/weather/Weather';
+import Notes from './components/Notes/Notes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather />
+        <Weather />        
+        <Notes />
       </header>
     </div>
   );
