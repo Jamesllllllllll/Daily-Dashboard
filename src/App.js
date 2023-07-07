ae6264284959ac3f5819668cafdb6585e839e0bc
musiked7 <95246508+musiked7@users.noreply.github.com>
2023-07-07 10:47:08 -0500

    3 Components, Redux Store, and reducers added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Weather from './features/weather/Weather';
 import './App.css';
+import { EmotionsCheckin } from './features/emotionsCheckin/EmotionsCheckin';
+import { EnergyCheckin } from './features/energyCheckin/EnergyCheckin';
+import { LineChart } from './features/lineChart/LineChart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Daily Dashboard App </h1>        
         <Weather />
       </header>
+      <EmotionsCheckin />
+      <EnergyCheckin />
+      <LineChart />
     </div>
   );
 }
