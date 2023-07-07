620181f0e0a931e4c2b5cbe05499e7fa659fba59
James <jkeezer@pm.me>
2023-07-07 14:44:15 -0400

    Revert "3 Components, Redux Store, and reducers added."
    This reverts commit ae6264284959ac3f5819668cafdb6585e839e0bc.

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import energyCheckinReducer from '../features/energyCheckin/energyCheckinSlice';
-import emotionsCheckinReducer from '../features/emotionsCheckin/emotionsCheckinSlice';
-import lineChartReducer from '../features/lineChart/lineChartSlice';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    emotionsCheckin: emotionsCheckinReducer,
-    energyCheckin: energyCheckinReducer,
-    lineChart: lineChartReducer,
+    counter: counterReducer,
   },
 });
