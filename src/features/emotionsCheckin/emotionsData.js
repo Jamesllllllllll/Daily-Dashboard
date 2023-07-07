0d0164ce80f1120b4e7b95f60d34b822a7dcfe82
musiked7 <95246508+musiked7@users.noreply.github.com>
2023-07-07 10:47:08 -0500

    3 Components, Redux Store, and reducers added.

---
+++ src/features/emotionsCheckin/emotionsData.js
@@ -0,0 +1,31 @@
+const emotionsArray = [
+    {
+        name: 'happy',
+        pic: '/media/Happy.png',
+    },
+    {
+        name: 'sad',
+        pic: '/media/Sad.png',
+    },
+    {
+        name: 'ok',
+        pic: '/media/OK.png',
+    },
+    {
+        name: 'meh',
+        pic: '/media/Meh.png',
+    },
+    {
+        name: 'anxious',
+        pic: '/media/Anxious.png',
+    },
+    {
+        name: 'angry',
+        pic: '/media/Angry.png',
+    }, 
+];
+
+
+export default emotionsArray;
+
+
