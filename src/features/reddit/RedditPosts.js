8d0993a7bfd7190b8339098105e551dc40219947
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-08 20:19:32 +1000

    Create RedditPosts.js
    Card stack of 5 popular reddit articles

--- src/features/reddit/RedditPosts.js
+++ src/features/reddit/RedditPosts.js
@@ -20,7 +20,49 @@ const RedditPosts = () => {
             <Stack spacing={2}>
                 {featuredPosts.map(({ id, url, description, author, title, image, publishedAt }) =>
                     <ArticleCard
+                        key={id}
                         id={id}
+                        url={url}
+                        title={title}
+                        description={description}
+                        author={author}
+                        image={image}
+                        publishedAt={publishedAt}
+                        isLoading={isLoading}
+                    />
+                )}
+                {/* } */}
+            </Stack>
+        </>
+    )
+};
+
+export default RedditPosts;
+
+
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRedditPosts, posts, isLoadingPosts } from './redditSlice';
+import ArticleCard from '../../components/ArticleCard/ArticleCard';
+import { Stack } from '@mui/material'
+
+const RedditPosts = () => {
+
+    const dispatch = useDispatch();
+    const featuredPosts = useSelector(posts);
+    const isLoading = useSelector(isLoadingPosts);
+
+    useEffect(() => {
+        dispatch(fetchRedditPosts());
+    }, [dispatch]);
+
+    return (
+        <>
+            <h1>Articles</h1>
+            <Stack spacing={2}>
+                {featuredPosts.map(({ id, url, description, author, title, image, publishedAt }) =>
+                    <ArticleCard
+                        key={id}
                         url={url}
                         title={title}
                         description={description}
