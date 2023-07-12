7f36b8f26a142132fe436c089b3f5ea1d9fddbbe
James <jkeezer@pm.me>
2023-07-12 00:48:04 -0400

    Redux Persist implemented

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Daily Dashboard App </h1>        
+        <h1>Daily Dashboard App </h1>
       </header>
       <Weather />
       <Notes />
