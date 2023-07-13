a8f1afc7f672542535b9f1852c1770050891d071
James <jkeezer@pm.me>
2023-07-12 22:42:11 -0400

    Set up React Router, Settings and Error pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import ErrorPage from "./error-page";
+import Settings from './routes/Settings';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />
+  }
+]);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
---
+++ src/routes/Settings.jsx
@@ -0,0 +1,12 @@
+export default function Settings() {
+    return (
+        <>
+        <h1>Settings</h1>
+        <ul>
+            <li>Setting 1</li>
+            <li>Setting 2</li>
+            <li>Setting 3</li>
+        </ul>
+        </>
+    )
+}
