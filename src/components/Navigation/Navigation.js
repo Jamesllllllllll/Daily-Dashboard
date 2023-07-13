1b704f669884dda2e25605e1989baa1014b2eb8c
James <jkeezer@pm.me>
2023-07-12 23:00:20 -0400

    Wrap Settings Icon Button in Link MUI component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,29 +4,24 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-    const handleOnClick = () => {
-        // Routing for settings 
 
-    }
-
-    return (
-            <AppBar position="sticky" sx={{marginBottom: '2rem'}} >
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Daily Dashboard
-                    </Typography>
-                    <IconButton 
-                        color="inherit"
-                        onClick={handleOnClick}
-                    >
-                        <SettingsIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-    );
-}
-
+  return (
+    <AppBar position="sticky" sx={{ marginBottom: '2rem' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Daily Dashboard
+        </Typography>
+        <Link to={`/settings`}>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navigation;
