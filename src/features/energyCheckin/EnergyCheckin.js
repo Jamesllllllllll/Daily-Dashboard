57082133e9ba4614a3409c2ff4340a7ffff5b481
musiked7 <95246508+musiked7@users.noreply.github.com>
2023-07-13 19:18:29 -0500

    working on redux thunk

--- src/features/energyCheckin/EnergyCheckin.js
+++ src/features/energyCheckin/EnergyCheckin.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
+  //I am importing the async actions from the slice reducer
   fetchEnergy,
+  saveEnergy
 } from './energyCheckinSlice';
 import styles from './EnergyCheckin.module.css';
 
 export function EnergyCheckin() {
+  //this is the state variable: energy
   const energy = useSelector(state => state.energyCheckin.todaysEnergy);
+  //I will use dispatch to call the actions
   const dispatch = useDispatch();
-/*   const storedEnergy = JSON.parse(localStorage.getItem('energy')); */
+  //not sure how to save to localStorage when calling the action saveEnergy
+  const storedEnergy = JSON.parse(localStorage.getItem('energy')); 
+  //Not sure how to find the value of the slider
   let slider = document.getElementById("energy-slider");
- /*  let sliderValue = slider.value; */
+  let sliderValue; 
 
   
-
+//this is what the user sees (slider);
   return (
     <>
       <div className={styles.sliderContainer}>
         <input name="energy-slider" type="range" min="1" max="100"  
-          className={styles.slider} value={energy} id="energy-slider" onChange={(event) => dispatch(fetchEnergy(event.target.value))}/>
+          className={styles.slider} value={energy} id="energy-slider" onChange={(event) => dispatch(fetchEnergy())}/>
         <div class={styles.energyValue}>{energy}</div>
       </div>
     </>
--- src/features/energyCheckin/energyCheckinSlice.js
+++ src/features/energyCheckin/energyCheckinSlice.js
@@ -1,42 +1,57 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-/* let slider = document.getElementById("energy-slider");
-let sliderValue = slider.value; */
+//How do I get the slider value???
+let slider = document.getElementById("energy-slider");
+let sliderValue;
 
-//Thunk Functions 
+//Thunk Functions:
+/*
+I am thinking I need two async functions...one to update the energy state
+and the other one to add the state to the history array (and save to local Storage?);
+*/
 export const fetchEnergy = createAsyncThunk(
   'energyCheckin/fetchEnergy', 
   async () => {
-    const en = await fetch(sliderValue).then(
+    const energy = await fetch(sliderValue).then(
     (data) => data.json()
   )
-  return en
+  return energy
 });
 
-/* export const saveEnergy = createAsyncThunk('energyCheckin/saveEnergy', async (value) => {
-  const addEnergy = await client.post('/fakeApi/todos', { })
-
-}); */
+export const saveEnergy = createAsyncThunk('energyCheckin/saveEnergy', async (value) => {
+ 
+});
 
 
 
 export const energyCheckinSlice = createSlice({
+  //name of Slice
   name: 'energyCheckin',
 
   initialState: {
+    //My only state variable for the energy component
     energy: 50,
   },
   reducers: {
-  
+    //I do not think I need regular reducers, just the async ones declared on extra reducers below:
   },
   extraReducers: builder => {
+    //As I understand, I need to add a case for each of the stages: pending, fulfilled and rejected.
+    //In each case I write the actions (logic) for each stage
+
+
     builder.addCase(fetchEnergy.pending, (state) => {
-        state.energy = 50;
+        state.energy = state.initialState;
       })
+      /*I want to update the energy state value here with whatever value
+        the user drops the slider at.
+      */
+
       .addCase(fetchEnergy.fulfilled, (state, action) => {
         const todaysEnergy = action.payload;
         state.energy = todaysEnergy;
       })
+      //Should I return an error? Can it be a string error message instead of a number as the state?
       .addCase(fetchEnergy.rejected, (state, action ) => {
         state.energy = action.payload;
       })
