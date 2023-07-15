b0fa0d116c7f364f7a97e9683e647e53a89065ea
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-07-15 17:35:25 +1000

    Temporary bug fix
    The default view point is not from the top. This issue emerged after the reddit component was implemented. This commit works around it by scrolling to top once everything is mounted. However, this does not solve the problem.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Home.module.css';
 import Navigation from '../components/Navigation/Navigation';
+import RedditPosts from '../features/reddit/RedditPosts';
 import Notes from '../features/notes/Notes';
 import Weather from '../features/weather/Weather';
 import { EmotionsCheckin } from '../features/emotionsCheckin/EmotionsCheckin';
@@ -8,11 +9,15 @@ import { EnergyCheckin } from '../features/energyCheckin/EnergyCheckin';
 import { LineChart } from '../features/lineChart/LineChart';
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <>
       <Navigation />
       <div className={styles.Home}>
         <Weather />
+        <RedditPosts />
         <Notes />
         <EmotionsCheckin />
         <EnergyCheckin />
