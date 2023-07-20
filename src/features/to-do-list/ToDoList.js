ad8f7c1998fcc6df69dbb6c8e7f82c9089b4a4f1
Dking <dking568@gmail.com>
2023-07-20 15:22:20 -0400

    Comment out the css import that is causing issues with Vercel

--- src/features/to-do-list/ToDoList.js
+++ src/features/to-do-list/ToDoList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+//import './App.css';
 import NewTask from './NewTask';
 import TaskList from './TaskList';
 import FullTaskDisplay from './FullTaskDisplay';
