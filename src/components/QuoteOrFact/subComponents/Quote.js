548400063a2f8822f14cd76c60eb34044c87d2ae
James <jkeezer@pm.me>
2023-08-11 15:24:37 -0400

    Add loading Skeleton to Quote and Fact

--- src/components/QuoteOrFact/subComponents/Quote.js
+++ src/components/QuoteOrFact/subComponents/Quote.js
@@ -1,29 +1,101 @@
-import React, { useState, useEffect } from 'react'
-
-
+import React, { useState, useEffect } from 'react';
+import { Skeleton, Box } from '@mui/material';
 //this component brings in 'quotes' from API Ninjas and returns them to App.js for rendering.
 function Quote() {
-    //define apikey, url to call, set headers and initialize quote and author
-  
+  //define apikey, url to call, set headers and initialize quote and author
+
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
+  const [loading, setLoading] = useState(true);
+  //the category array is comprised of the list of possible quote categories provided by API Ninjas
+  const category = [
+    'age',
+    'alone',
+    'amazing',
+    'anger',
+    'architecture',
+    'art',
+    'attitude',
+    'beauty',
+    'best',
+    'birthday',
+    'business',
+    'car',
+    'change',
+    'communications',
+    'computers',
+    'cool',
+    'courage',
+    'dad',
+    'dating',
+    'death',
+    'design',
+    'dreams',
+    'education',
+    'environmental',
+    'equality',
+    'experience',
+    'failure',
+    'faith',
+    'family',
+    'famous',
+    'fear',
+    'fitness',
+    'food',
+    'forgiveness',
+    'freedom',
+    'friendship',
+    'funny',
+    'future',
+    'god',
+    'good',
+    'government',
+    'graduation',
+    'great',
+    'happiness',
+    'health',
+    'history',
+    'home',
+    'hope',
+    'humor',
+    'imagination',
+    'inspirational',
+    'intelligence',
+    'jealousy',
+    'knowledge',
+    'leadership',
+    'learning',
+    'legal',
+    'life',
+    'love',
+    'marriage',
+    'medical',
+    'men',
+    'mom',
+    'money',
+    'morning',
+    'movies',
+    'success',
+  ];
 
-    //the category array is comprised of the list of possible quote categories provided by API Ninjas
-  const category=["age", "alone", "amazing", "anger", "architecture", "art", "attitude", "beauty", "best", "birthday", "business", "car", "change", "communications", "computers", "cool", "courage", "dad", "dating", "death", "design", "dreams", "education", "environmental", "equality", "experience", "failure", "faith", "family", "famous", "fear", "fitness", "food", "forgiveness", "freedom", "friendship", "funny", "future", "god", "good", "government", "graduation", "great", "happiness", "health", "history", "home", "hope", "humor", "imagination", "inspirational", "intelligence", "jealousy", "knowledge", "leadership", "learning", "legal", "life", "love", "marriage", "medical", "men", "mom", "money", "morning", "movies", "success"]
+  //defining a number to act as an index so that the quote category can be randomized.  In the future, may add functionality to turn on or select the categories to recieve quotes from.
+  const categoryIndex = Math.floor(Math.random() * category.length);
 
-    //defining a number to act as an index so that the quote category can be randomized.  In the future, may add functionality to turn on or select the categories to recieve quotes from.
-  const categoryIndex =Math.floor(Math.random() * category.length)
-    
-    //calling the API and setting quote and author to the response which comes back as an array object. you can find more information here https://api-ninjas.com/api/facts
+  //calling the API and setting quote and author to the response which comes back as an array object. you can find more information here https://api-ninjas.com/api/facts
   const fetchQuote = async () => {
+    setLoading(true);
     try {
-      const response = await fetch(`/api/quote?category=${category[categoryIndex]}`);
+      const response = await fetch(
+        `/api/quote?category=${category[categoryIndex]}`
+      );
       if (response.ok) {
         const data = await response.json();
         setQuote(data[0].quote);
-        setAuthor(data[0].author)
+        setAuthor(data[0].author);
+        setLoading(false);
       } else {
         console.log(`Error: ${response.statusText}`);
+        setLoading(false);
         return <p>No quote data</p>;
       }
     } catch (error) {
@@ -32,22 +104,59 @@ function Quote() {
   };
 
   useEffect(() => {
-    fetchQuote()
-  }, [])
+    fetchQuote();
+  }, []);
 
-    //link variable is defined to pull a wikipedia article for the returned author. No special syntax was given, url  auto corrects for spacing within author
-  const link = `https://en.wikipedia.org/wiki/${author}`
+  //link variable is defined to pull a wikipedia article for the returned author. No special syntax was given, url  auto corrects for spacing within author
+  const link = `https://en.wikipedia.org/wiki/${author}`;
   //console.log(link);
   //console.log('quote ' + quote)
 
-    //Returning quote and author to be exported to App.js. The link for author is set to 'link' and the target is set to blank so that when the fact is clicked it will open a new tab in the browser with the specific Wikipedia article for that author
+  //Returning quote and author to be exported to App.js. The link for author is set to 'link' and the target is set to blank so that when the fact is clicked it will open a new tab in the browser with the specific Wikipedia article for that author
   return (
     <>
-      <p> {quote} </p>
-      <p>-<a id="author" href={link} target="blank">{author}</a> </p>
+      {!loading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'start',
+            textAlign: 'left'
+          }}
+        >
+          <p> {quote} </p>
+          <p>
+            -
+            <a id="author" href={link} target="blank">
+              {author}
+            </a>{' '}
+          </p>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'start',
+          }}
+        >
+          <Skeleton
+            variant="rounded"
+            height={'1.25rem'}
+            animation="wave"
+            sx={{ mb: '1.75rem' }}
+          />
+          <Skeleton
+            variant="rounded"
+            height={'1.25rem'}
+            width={150}
+            animation="wave"
+            sx={{ m: 0 }}
+          />
+        </Box>
+      )}
     </>
-    
-  )
+  );
 }
 
 export default Quote;
