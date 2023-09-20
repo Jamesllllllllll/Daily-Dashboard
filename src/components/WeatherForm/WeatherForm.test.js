7743c2bd5ab444559290a4b919353ba8ec24b7a8
James <jkeezer@pm.me>
2023-09-19 23:56:32 -0400

    MSW working for /api routes

--- src/components/WeatherForm/WeatherForm.test.js
+++ src/components/WeatherForm/WeatherForm.test.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { rest } from 'msw';
-import server from '../../mocks/server';
+
+// import API mocking utilities from Mock Service Worker ({ rest } already imported in handlers.js)
+import { server } from '../../mocks/server';
+
+// import react-testing methods
 import { fireEvent, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../utils/test-utils';
-import WeatherForm from './WeatherForm';
+import userEvent from '@testing-library/user-event';
+
+// add custom jest matchers from jest-dom
 import '@testing-library/jest-dom';
 
-//-----------------------
-// None of these are required since we are using renderWithProviders! - James
-//-----------------------
-//
-// import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
-// import weatherReducer from '../../features/weather/weatherSlice';
-// import {
-//   updateCity,
-// } from '../../features/weather/weatherSlice';
-// import { changeCitySelector, changeCity } from '../../routes/settingsSlice';
-//
-//-----------------------
+// the component to test
+import WeatherForm from './WeatherForm';
+
+// establish API mocking before all tests
+beforeAll(() => server.listen());
+// reset any request handlers that are declared as a part of our tests
+// (i.e. for testing one-time error scenarios)
+afterEach(() => server.resetHandlers());
+// clean up once the tests are done
+afterAll(() => server.close());
 
 it('Should show text content as Toronto', async () => {
   // set up userEvent before component is rendered (https://testing-library.com/docs/user-event/intro#writing-tests-with-userevent)
@@ -30,16 +31,16 @@ it('Should show text content as Toronto', async () => {
   // Extract the textbox component
   const textbox = screen.getByRole('textbox');
   // Simulate typing 'Toronto'
-  user.type(textbox, 'Toronto');
+  user.type(textbox, 'Atlanta');
   // Assert textbox has text content 'Toronto'
-  const city = await screen.findByDisplayValue('Toronto');
+  const city = await screen.findByDisplayValue('Atlanta');
   expect(city).toBeInTheDocument();
 });
 
 it('fetches & receives a list of cities as the user types', async () => {
-	renderWithProviders(<WeatherForm />)
+  renderWithProviders(<WeatherForm />);
 
-	const textbox = screen.getByRole('textbox');
+  const textbox = screen.getByRole('textbox');
 
-	expect(textbox.value).toBe('');
-})
+  expect(textbox.value).toBe('');
+});
