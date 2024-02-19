1261b33bf94f56577b0ee7010f35f6e6f80d3ce9
James <jkeezer@pm.me>
2024-02-18 19:12:58 -0500

    Updates to styling and fix energy history bug

--- src/features/emotionsCheckin/EmotionsCheckin.js
+++ src/features/emotionsCheckin/EmotionsCheckin.js
@@ -1,75 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   selectEmotion,
   removeEmotionFromHistory,
   addToEmotionsHistory,
 } from './emotionsCheckinSlice';
-import emotionsArray  from './emotionsData';
+import emotionsArray from './emotionsData';
 
 import styles from './EmotionsCheckin.module.css';
 
 //This component returns the UI for the emojis section.
 export default function EmotionsCheckin() {
-//state variables:  
-const emotionName = useSelector((state) => state.emotionsCheckin.todaysEmotion.name);
-const emotionPic = useSelector((state) => state.emotionsCheckin.todaysEmotion.pic);
-const latestDateStored = useSelector(state => state.emotionsCheckin.emotionsHistory.at(-1).date);
+  //state variables:
+  const emotionName = useSelector(
+    (state) => state.emotionsCheckin.todaysEmotion.name
+  );
+  const emotionPic = useSelector(
+    (state) => state.emotionsCheckin.todaysEmotion.pic
+  );
+  const latestDateStored = useSelector(
+    (state) => state.emotionsCheckin.emotionsHistory.at(-1).date
+  );
 
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-//handleClick dispatches actions
-//target is button
-function handleClick(e) {
-  let date = new Date();
-  let today = date.getDay();
-  const picName = e.target.name;
-  const picSrc = e.target.src;
+  const [selectOpen, setSelectOpen] = useState(false);
 
-  dispatch(selectEmotion({
-    name: picName,
-    pic: picSrc,
-  }));
+  //handleClick dispatches actions
+  //target is button
+  function handleClick(e) {
+    let date = new Date();
+    let today = date.getDay();
+    const picName = e.target.name;
+    const picSrc = e.target.src;
+    setSelectOpen(!selectOpen)
+    dispatch(
+      selectEmotion({
+        name: picName,
+        pic: picSrc,
+      })
+    );
 
-  if (today === latestDateStored){
-    dispatch(removeEmotionFromHistory())
+    if (today === latestDateStored) {
+      dispatch(removeEmotionFromHistory());
+    }
+
+    dispatch(
+      addToEmotionsHistory({
+        date: today,
+        name: picName,
+        pic: picSrc,
+      })
+    );
   }
 
-  dispatch(addToEmotionsHistory({
-    date: today,
-    name: picName,
-    pic: picSrc,
-  }));
-} 
+  //emojiPics is an array of emoji buttons
+  let emojiPics = emotionsArray.map(({ name, pic }) => {
+    return (
+      <li key={name}>
+        <button
+          value={name}
+          className={`${styles.button} ${styles.listButton}`}
+        >
+          <img
+            alt={name}
+            name={name}
+            src={pic}
+            onClick={handleClick}
+            className={styles.img}
+          />
+        </button>
+      </li>
+    );
+  });
 
-//emojiPics is an array of emoji buttons
-let emojiPics = emotionsArray.map( ({name, pic}) => {
   return (
-   <li key={name}> 
-    <button value={name} className={styles.button}>
-      <img alt={name} name={name} src={pic} onClick={handleClick} className={styles.img} />
-    </button>
-   </li>
-  )
-})
- 
-   
-  return (
-    <>
-      <div className={styles.container}>
         <div className={styles.emotionsContainer}>
-          <ul className={styles.list}>
-            {emojiPics}
-          </ul>
-
-          <button className={styles.openingEmojiButton}>
-            <img src={emotionPic} alt={emotionName} className={styles.emojiPic}/>
+          <button className={styles.button} onClick={() => setSelectOpen(!selectOpen)}>
+            <img
+              src={emotionPic}
+              alt={emotionName}
+              className={styles.emojiPic}
+            />
           </button>
+          {selectOpen && <ul className={styles.list}>{emojiPics}</ul>}
         </div>
-      </div> 
-    </>  
   );
 }
-
-  
-  
--- src/features/energyCheckin/EnergyCheckin.js
+++ src/features/energyCheckin/EnergyCheckin.js
@@ -6,14 +6,17 @@ import {
   removeEnergyFromHistory,
   addToHistory,
 } from './energyCheckinSlice';
-import lightning from "../../../src/Lightning.png"
+import lightning from '../../../src/Lightning.png';
 import styles from './EnergyCheckin.module.css';
+import { Button } from '@mui/material';
 
 export default function EnergyCheckin() {
   //state variables:
-  const sliderOpen = useSelector(state => state.energyCheckin.sliderOpen);
-  const energy = useSelector(state => state.energyCheckin.todaysEnergy);
-  const latestDateStored = useSelector(state => state.energyCheckin.energyHistory.at(-1).date);
+  const sliderOpen = useSelector((state) => state.energyCheckin.sliderOpen);
+  const energy = useSelector((state) => state.energyCheckin.todaysEnergy);
+  const latestDateStored = useSelector(
+    (state) => state.energyCheckin.energyHistory.at(-1).date
+  );
   const dispatch = useDispatch();
 
   function handleChange(e) {
@@ -22,40 +25,50 @@ export default function EnergyCheckin() {
     const energyValue = e.target.value;
 
     dispatch(selectEnergy(energyValue));
-  
-    if (today === latestDateStored){
-      dispatch(removeEnergyFromHistory())
+
+    if (today === latestDateStored) {
+      dispatch(removeEnergyFromHistory());
     }
 
-    dispatch(addToHistory({
-      date: today,
-      energy: energyValue,
-    })); 
-  }  
+    dispatch(
+      addToHistory({
+        date: today,
+        energy: energyValue,
+      })
+    );
+  }
 
-
-//check sliderOpen state and display either slider or lightning picture:
+  //check sliderOpen state and display either slider or lightning picture:
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.energyContainer}>
-          <button className={styles.energyButton} onClick={() => dispatch(toggleSlider())}>
-            <img src={lightning} alt="lightning" className={styles.lightning}/>
-          </button>
-        </div>
-      </div>
-      {sliderOpen && 
+    <div className={styles.energyContainer}>
+      <button
+        className={styles.button}
+        onClick={() => dispatch(toggleSlider())}
+      >
+        <img src={lightning} alt='lightning' className={styles.lightning} />
+      </button>
+      {sliderOpen && (
         <div className={styles.sliderContainer}>
-          <input name="energy-slider" type="range" min="0" max="100" step="10" 
-          className={styles.slider} value={energy} id="energy-slider" onChange={handleChange}/>
-          <p className={styles.energyValue}>{energy}</p> 
-          <button className={styles.closeButton} onClick={() => dispatch(toggleSlider())}>Close</button>
+          <input
+            name='energy-slider'
+            type='range'
+            min='0'
+            max='100'
+            step='10'
+            className={styles.slider}
+            value={energy}
+            id='energy-slider'
+            onChange={handleChange}
+          />
+          <p className={styles.energyValue}>{energy}</p>
+          <Button
+            onClick={() => dispatch(toggleSlider())}
+            sx={{ alignSelf: 'center' }}
+          >
+            Save
+          </Button>
         </div>
-      }
-    </> 
-  )
+      )}
+    </div>
+  );
 }
-
-
-
-
